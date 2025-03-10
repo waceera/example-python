79d29e6fd332a017d31e4f4acd884ec757d70f37
ejeng roza <128566448+waceera@users.noreply.github.com>
2025-03-10 17:26:53 +0700

    Update q.py

--- q.py
+++ q.py
@@ -1,8 +1,8 @@
-ngising = input("Apakah anda kebelet berak? (hooh/tak)\ncontainer@pikok:")
+ngising = input("🌿 Apakah anda kebelet berak? (hooh/tak)\ncontainer@pikok: ")
 if ngising == "hooh":
-  kmr_mnd = input("Apakah kamar mandinya sedang digunakan/sedang dalam perbaikan? (hooh/tak)\ncontainer@pikok:")
+  kmr_mnd = input("🌿 Apakah kamar mandinya sedang digunakan/sedang dalam perbaikan? (hooh/tak)\ncontainer@pikok: ")
   if kmr_mnd == "hooh":
-    print("Ngeden aja dulu wak (⁠≧⁠▽⁠≦⁠)")
+    print("🤖 Ngeden aja dulu wak (⁠≧⁠▽⁠≦⁠)")
   else:
-    print("Bemrak ajah")
+    print("🤖 Bemrak ajah")
     
